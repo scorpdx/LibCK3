85be0b68f23dda2423785f7ac845c7fb52712286
J. Zebedee <zebedee@code.gripe>
2021-01-20 15:55:37 -0500

    Add BinFragment and generator stubs

---
+++ test/LibCK3.Generator/BinFragment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Buffers;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibCK3.Generator
+{
+    public class BinFragment : ReadOnlySequenceSegment<byte>
+    {
+        private readonly ReadOnlySequenceSegment<byte> _start;
+
+        private BinFragment(ReadOnlySequenceSegment<byte> start, ReadOnlyMemory<byte> memory) : this(memory)
+        {
+            _start = start;
+        }
+        public BinFragment(ReadOnlyMemory<byte> memory) : base()
+        {
+            Memory = memory;
+        }
+
+        public BinFragment Append(ReadOnlyMemory<byte> memory)
+        {
+            var segment = new BinFragment(_start ?? this, memory)
+            {
+                RunningIndex = RunningIndex + Memory.Length
+            };
+
+            Next = segment;
+            return segment;
+        }
+
+        public ReadOnlySequence<byte> Build() => new(_start, 0, this, Memory.Length);
+    }
+}
---
+++ test/LibCK3.Generator/BinFragmentExtensions.cs
@@ -0,0 +1,22 @@
+﻿using LibCK3.Parsing;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibCK3.Generator
+{
+    public static class BinFragmentExtensions
+    {
+        private static BinFragment Append(this BinFragment fragment, SpecialTokens token)
+            => fragment.Append(BitConverter.GetBytes((ushort)token));
+
+        public static BinFragment Open(this BinFragment fragment)
+            => fragment.Append(SpecialTokens.Open);
+
+        public static BinFragment Close(this BinFragment fragment)
+            => fragment.Append(SpecialTokens.Close);
+    }
+}
