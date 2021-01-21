7c555721f0528af7918320bf13033c4b3a87aed5
J. Zebedee <zebedee@code.gripe>
2021-01-21 12:27:19 -0500

    Add GenerateIntArray test to GeneratorTests

--- test/LibCK3.Tests/GeneratorTests.cs
+++ test/LibCK3.Tests/GeneratorTests.cs
@@ -16,15 +16,34 @@ namespace LibCK3.Tests
         private static readonly byte[] Close = BitConverter.GetBytes((ushort)SpecialTokens.Close);
 
         [Fact]
-        public void GenerateEmptyObject()
+        public void GenerateEmptyContainer()
         {
             var frag = new BinFragment();
             var seq = frag.Open().Close().Build();
 
             Assert.Equal(
                 seq.ToArray(),
-                /* {} */
-                new byte[] { 0x03, 0x00, 0x04, 0x00 });
+                new byte[] {
+                    /*{*/0x03, 0x00,
+                    /*}*/0x04, 0x00
+                });
+        }
+
+        [Fact]
+        public void GenerateIntArray()
+        {
+            var frag = new BinFragment();
+            var seq = frag.Open().Int(1).Int(2).Int(3).Close().Build();
+
+            Assert.Equal(
+                seq.ToArray(),
+                new byte[] {
+                    /*{*/0x03, 0x00,
+                    /*int*/0xC, 0x00, /*1*/ 0x01, 0x00, 0x00, 0x00,
+                    /*int*/0xC, 0x00, /*2*/ 0x02, 0x00, 0x00, 0x00,
+                    /*int*/0xC, 0x00, /*3*/ 0x03, 0x00, 0x00, 0x00,
+                    /*}*/0x04, 0x00
+                });
         }
     }
 }
