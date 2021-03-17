bd6ceacee4aedeaf88ad4b26bc137909b66ecc2c
J. Zebedee <zebedee@code.gripe>
2021-03-17 12:42:38 -0500

    Use CK3 save instead of standalone gamestate zip

--- test/LibCK3.Tests/GeneratorTests.cs
+++ test/LibCK3.Tests/GeneratorTests.cs
@@ -1,20 +1,12 @@
 ﻿using LibCK3.Generator;
-using LibCK3.Parsing;
-using System;
 using System.Buffers;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace LibCK3.Tests
 {
     public class GeneratorTests
     {
-        private static readonly byte[] Open = BitConverter.GetBytes((ushort)SpecialTokens.Open);
-        private static readonly byte[] Close = BitConverter.GetBytes((ushort)SpecialTokens.Close);
-
         [Fact]
         public void GenerateEmptyContainer()
         {
