04da2d24beb1367795673915e5345a17b972b656
J. Zebedee <zebedee@code.gripe>
2021-03-17 12:42:59 -0500

    Don't pack Generator

--- test/LibCK3.Generator/BinFragmentExtensions.cs
+++ test/LibCK3.Generator/BinFragmentExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using LibCK3.Parsing;
 using LibCK3.Tokens;
 using System;
-using System.Buffers.Text;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LibCK3.Generator
 {
