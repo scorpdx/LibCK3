e796a1c791aa11816c98f12b6d33eb1a03e75cc4
J. Zebedee <zebedee@code.gripe>
2021-06-09 01:34:31 -0500

    Break out overlays into ValueOverlays

--- src/Parsing/CK3Token.cs
+++ src/Parsing/CK3Token.cs
@@ -1,7 +1,5 @@
 using LibCK3.Tokens;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Text.Json;
 
 namespace LibCK3.Parsing
@@ -42,48 +40,6 @@ namespace LibCK3.Parsing
         public JsonEncodedText AsIdentifier() => CK3Tokens.Tokens[ID];
 
         public ValueOverlayFlags GetOverlay()
-            => _overlays.TryGetValue(ID, out var overlayFlags) ? overlayFlags : ValueOverlayFlags.None;
-
-        private static readonly Dictionary<ushort, ValueOverlayFlags> _overlays = (new Dictionary<string, ValueOverlayFlags>()
-        {
-            { "date", ValueOverlayFlags.AsDate },
-            { "meta_date", ValueOverlayFlags.AsDate },
-            { "meta_real_date", ValueOverlayFlags.AsDate },
-            { "bookmark_date", ValueOverlayFlags.AsDate },
-            { "start_time", ValueOverlayFlags.AsDate },
-            { "start_date", ValueOverlayFlags.AsDate },
-            { "end_date", ValueOverlayFlags.AsDate },
-            { "expiration_date", ValueOverlayFlags.AsDate },
-            { "history", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren },
-            { "reign_opinion_held_since", ValueOverlayFlags.AsDate },
-            { "found_date", ValueOverlayFlags.AsDate },
-            { "birth", ValueOverlayFlags.AsDate },
-            { "became_ruler_date", ValueOverlayFlags.AsDate },
-            { "pool_history", ValueOverlayFlags.AsDate },
-            { "leave_court_date", ValueOverlayFlags.AsDate },
-            { "decision_cooldowns", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren  },
-            { "imprison_type_date", ValueOverlayFlags.AsDate },
-            { "last_war_finish_date", ValueOverlayFlags.AsDate },
-            { "arrival_date", ValueOverlayFlags.AsDate },
-            { "spawn_date", ValueOverlayFlags.AsDate },
-            { "scheme_cooldowns_0", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
-            { "scheme_cooldowns_1", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
-            { "cooldown_against_recipient_0", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
-            { "cooldown_against_recipient_1", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
-            { "cooldown", ValueOverlayFlags.AsDate },
-            { "last_supply_date", ValueOverlayFlags.AsDate },
-            { "gathered_date", ValueOverlayFlags.AsDate },
-            { "last_councillor_change", ValueOverlayFlags.AsDate },
-            { "hired_until", ValueOverlayFlags.AsDate },
-            { "force", ValueOverlayFlags.AsDate },
-            { "dates", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren },
-            { "last_action", ValueOverlayFlags.AsDate },
-            { "atrtition_date", ValueOverlayFlags.AsDate },
-            //{ "", ValueOverlayFlags.AsDate },
-            { "gold", ValueOverlayFlags.AsQ },
-            //{ "", ValueOverlayFlags.AsQ },
-        })
-            .Join(CK3Tokens.Tokens, okvp => okvp.Key, ikvp => ikvp.Value.ToString(), (ok, ik) => (ik.Key, ok.Value))
-            .ToDictionary(tup => tup.Key, tup => tup.Value);
+            => ValueOverlays.Overlays.TryGetValue(ID, out var overlayFlags) ? overlayFlags : ValueOverlayFlags.None;
     }
 }
---
+++ src/Parsing/ValueOverlays.cs
@@ -0,0 +1,51 @@
+﻿using LibCK3.Tokens;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibCK3.Parsing
+{
+    public static class ValueOverlays
+    {
+        public static IReadOnlyDictionary<ushort, ValueOverlayFlags> Overlays { get; } = new Dictionary<string, ValueOverlayFlags>()
+        {
+            { "date", ValueOverlayFlags.AsDate },
+            { "meta_date", ValueOverlayFlags.AsDate },
+            { "meta_real_date", ValueOverlayFlags.AsDate },
+            { "bookmark_date", ValueOverlayFlags.AsDate },
+            { "start_time", ValueOverlayFlags.AsDate },
+            { "start_date", ValueOverlayFlags.AsDate },
+            { "end_date", ValueOverlayFlags.AsDate },
+            { "expiration_date", ValueOverlayFlags.AsDate },
+            { "history", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren },
+            { "reign_opinion_held_since", ValueOverlayFlags.AsDate },
+            { "found_date", ValueOverlayFlags.AsDate },
+            { "birth", ValueOverlayFlags.AsDate },
+            { "became_ruler_date", ValueOverlayFlags.AsDate },
+            { "pool_history", ValueOverlayFlags.AsDate },
+            { "leave_court_date", ValueOverlayFlags.AsDate },
+            { "decision_cooldowns", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren  },
+            { "imprison_type_date", ValueOverlayFlags.AsDate },
+            { "last_war_finish_date", ValueOverlayFlags.AsDate },
+            { "arrival_date", ValueOverlayFlags.AsDate },
+            { "spawn_date", ValueOverlayFlags.AsDate },
+            { "scheme_cooldowns_0", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
+            { "scheme_cooldowns_1", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
+            { "cooldown_against_recipient_0", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
+            { "cooldown_against_recipient_1", ValueOverlayFlags.AsDate | ValueOverlayFlags.KeepForChildren },
+            { "cooldown", ValueOverlayFlags.AsDate },
+            { "last_supply_date", ValueOverlayFlags.AsDate },
+            { "gathered_date", ValueOverlayFlags.AsDate },
+            { "last_councillor_change", ValueOverlayFlags.AsDate },
+            { "hired_until", ValueOverlayFlags.AsDate },
+            { "force", ValueOverlayFlags.AsDate },
+            { "dates", ValueOverlayFlags.AsDate | ValueOverlayFlags.Repeats | ValueOverlayFlags.KeepForChildren },
+            { "last_action", ValueOverlayFlags.AsDate },
+            { "atrtition_date", ValueOverlayFlags.AsDate },
+            //{ "", ValueOverlayFlags.AsDate },
+            { "gold", ValueOverlayFlags.AsQ },
+            //{ "", ValueOverlayFlags.AsQ },
+        }
+        .Join(CK3Tokens.Tokens, okvp => okvp.Key, ikvp => ikvp.Value.ToString(), (ok, ik) => (ik.Key, ok.Value))
+        .ToDictionary(tup => tup.Key, tup => tup.Value);
+    }
+}
