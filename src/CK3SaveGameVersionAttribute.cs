a2ea65bc43bdf5a3b3b5cf198229ec12530096a9
J. Zebedee <zebedee@code.gripe>
2021-10-19 13:37:00 -0500

    Add CK3SaveGameVersionAttribute

---
+++ src/CK3SaveGameVersionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LibCK3
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    internal class CK3SaveGameVersionAttribute : Attribute
+    {
+        public string SaveGameVersion { get; }
+
+        public CK3SaveGameVersionAttribute(string saveGameVersion)
+        {
+            this.SaveGameVersion = saveGameVersion;
+        }
+    }
+}
