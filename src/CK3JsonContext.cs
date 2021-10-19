6fc4309b2a1c7665a363ea6e94dd158cdcaf4d6d
J. Zebedee <zebedee@code.gripe>
2021-10-19 13:37:40 -0500

    Add CK3JsonContext for source generation

---
+++ src/CK3JsonContext.cs
@@ -0,0 +1,14 @@
+﻿using LibCK3.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace LibCK3
+{
+    [JsonSerializable(typeof(Meta_Data))]
+    [JsonSerializable(typeof(Gamestate))]
+
+    [JsonSerializable(typeof(ActiveSchemeDerived))]
+    internal partial class CK3JsonContext : JsonSerializerContext
+    {
+    }
+}
